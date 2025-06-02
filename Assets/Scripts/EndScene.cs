54e54be15f8819f73caf71a7ad26c4a898dc5e48
Dev Patel <devp2@uci.edu>
2025-06-01 18:16:58 -0700

    Add placeholder functionality for win game (end scene)

---
+++ Assets/Scripts/EndScene.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class EndScene : MonoBehaviour
+{
+    [SerializeField] private GameObject[] partsArray = new GameObject[3];
+    [SerializeField] private Button nextButton;
+
+    private int curPartIndex = 0;
+
+    private void OnEnable()
+    {
+        foreach (GameObject part in partsArray)
+        {
+            part.SetActive(false);
+        }
+        if (partsArray.Length > 0)
+        {
+            partsArray[0].SetActive(true);
+        }
+    }
+
+    public void NextPart()
+    {
+        if (curPartIndex < partsArray.Length)
+        {
+            partsArray[curPartIndex].SetActive(false);
+            curPartIndex++;
+            if (curPartIndex < partsArray.Length)
+            {
+                partsArray[curPartIndex].SetActive(true);
+                if (curPartIndex == partsArray.Length - 1)
+                {
+                    nextButton.onClick.RemoveAllListeners();
+                    nextButton.onClick.AddListener(EndGame);
+                }
+            }
+        }
+    }
+
+    public void EndGame()
+    {
+        SceneManager.LoadScene(Globals.titleSceneName);
+    }
+}
